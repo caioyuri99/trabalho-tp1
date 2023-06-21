62b88f9871d80ba77ef806eb46e924ef94a22c8f
Zanarck <caioyuri1000@gmail.com>
2023-06-21 15:32:48 -0300

    put main class in main package

--- src/main/App.java
+++ src/main/App.java
@@ -1,3 +1,5 @@
+package main;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -5,15 +7,17 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class App extends Application {
-    public static void main(String[] args) throws Exception {
-        launch(args);
-    }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("./telas/TelaInicial.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("./../telas/TelaInicial.fxml"));
 
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
+
+    public static void main(String[] args) throws Exception {
+        launch(args);
+    }
+
 }
