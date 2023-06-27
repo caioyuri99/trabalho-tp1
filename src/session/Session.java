c54f159a6deaafd8d97fcb737c47ce35f23c13e1
Zanarck <caioyuri1000@gmail.com>
2023-06-27 11:16:18 -0300

    refactor class

--- src/session/Session.java
+++ src/session/Session.java
@@ -1,9 +1,13 @@
 package session;
 
+import java.time.LocalDate;
+
+import biblioteca.Cliente;
 import biblioteca.Usuario;
 
 public class Session {
     private static Usuario loggedUser = null;
+    private static LocalDate dataAtual = LocalDate.now();
 
     public static boolean isLogged() {
         return loggedUser != null;
@@ -13,7 +17,29 @@ public class Session {
         return loggedUser;
     }
 
-    public static void setLoggedUser(Usuario loggedUser) {
-        Session.loggedUser = loggedUser;
+    public static void login(Usuario usuario) throws Exception {
+        loggedUser = usuario;
+
+        if (usuario instanceof Cliente) {
+            ((Cliente) usuario).calcularMulta();
+        }
+    }
+
+    public static void logout() {
+        loggedUser = null;
+    }
+
+    public static void verificaEmprestimos() throws Exception {
+        if (loggedUser instanceof Cliente) {
+            ((Cliente) loggedUser).calcularMulta();
+        }
+    }
+
+    public static LocalDate getDataAtual() {
+        return dataAtual;
+    }
+
+    public static void setDataAtual(LocalDate dataAtual) {
+        Session.dataAtual = dataAtual;
     }
 }
