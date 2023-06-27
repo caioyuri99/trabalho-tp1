d7a30edae6261f8c588e73f03feca1aacebef02b
Zanarck <caioyuri1000@gmail.com>
2023-06-27 11:06:49 -0300

    create gerenciarEstantes screen (not implemented)

---
+++ src/controllers/GerenciarEstantes.java
@@ -0,0 +1,21 @@
+package controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+
+public class GerenciarEstantes {
+
+    @FXML
+    private Button add;
+
+    @FXML
+    private Button delete;
+
+    @FXML
+    private Button edit;
+
+    @FXML
+    private ListView<?> listEstantes;
+
+}
