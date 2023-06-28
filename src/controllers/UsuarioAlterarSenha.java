ec520907e73249dea0261ed75f3e5c5f279ada0f
Zanarck <caioyuri1000@gmail.com>
2023-06-28 17:25:05 -0300

    generalize to Usuario class

--- src/controllers/UsuarioAlterarSenha.java
+++ src/controllers/UsuarioAlterarSenha.java
@@ -1,68 +1,45 @@
 package controllers;
 
-import java.net.URL;
 import java.util.Optional;
-import java.util.ResourceBundle;
 
-import biblioteca.Cliente;
+import biblioteca.Usuario;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.ButtonBar.ButtonData;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import session.Session;
 
-public class DadosClientePopup implements Initializable {
+public class UsuarioAlterarSenha {
 
     @FXML
-    private TextField nomeCliente;
+    private PasswordField novaSenhaUser;
 
     @FXML
-    private PasswordField novaSenhaCliente;
-
-    @FXML
-    private PasswordField senhaCliente;
-
-    @Override
-    public void initialize(URL arg0, ResourceBundle arg1) {
-        String nomeCliente = Session.getLoggedUser().getNome();
-        this.nomeCliente.setText(nomeCliente);
-    }
+    private PasswordField senhaUser;
 
     @FXML
     void alterarDados(ActionEvent event) {
-        String nome = nomeCliente.getText();
-        String senha = senhaCliente.getText();
-        String novaSenha = novaSenhaCliente.getText();
+        String senha = senhaUser.getText();
+        String novaSenha = novaSenhaUser.getText();
 
-        if (nome.isEmpty() && novaSenha.isEmpty()) {
+        if (novaSenha.isEmpty()) {
             Alert alert = new Alert(AlertType.ERROR);
             alert.setTitle("Erro");
             alert.setHeaderText("Erro ao alterar dados");
-            alert.setContentText("Nenhum campo foi preenchido");
-            alert.showAndWait();
-            return;
-        }
-
-        if (!senha.equals(Session.getLoggedUser().getSenha())) {
-            Alert alert = new Alert(AlertType.ERROR);
-            alert.setTitle("Erro");
-            alert.setHeaderText("Erro ao alterar dados");
-            alert.setContentText("Senha incorreta");
+            alert.setContentText("Nova senha não pode ser vazia");
             alert.showAndWait();
             return;
         }
 
         Alert alert = new Alert(AlertType.CONFIRMATION);
         alert.setTitle("Confirmação");
-        alert.setHeaderText("Confirmação de alteração de dados");
-        alert.setContentText("Deseja realmente alterar seus dados?");
+        alert.setHeaderText("Confirmação de alteração de senha");
+        alert.setContentText("Deseja realmente alterar sua senha?");
 
         ButtonType buttonConfirmar = new ButtonType("Confirmar", ButtonData.OK_DONE);
         ButtonType buttonCancelar = new ButtonType("Cancelar", ButtonData.CANCEL_CLOSE);
@@ -70,16 +47,11 @@ public class DadosClientePopup implements Initializable {
 
         Optional<ButtonType> result = alert.showAndWait();
         if (result.get() == buttonConfirmar) {
-            Cliente cliente = (Cliente) Session.getLoggedUser();
-
-            if (!nome.isEmpty())
-                cliente.setNome(nome);
-            if (!novaSenha.isEmpty())
-                cliente.setSenha(novaSenha);
+            Usuario usuario = Session.getLoggedUser();
 
             try {
-                cliente.atualizarCadastro();
-                Session.login(cliente);
+                usuario.alterarSenha(senha, novaSenha);
+                Session.login(usuario);
             } catch (Exception e) {
                 Alert alertErro = new Alert(AlertType.ERROR);
                 alertErro.setTitle("Erro");
@@ -91,7 +63,7 @@ public class DadosClientePopup implements Initializable {
 
             Alert alertSucesso = new Alert(AlertType.INFORMATION);
             alertSucesso.setTitle("Sucesso");
-            alertSucesso.setHeaderText("Dados alterados com sucesso");
+            alertSucesso.setHeaderText("Senha alterada com sucesso");
             alertSucesso.showAndWait();
             Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
             stage.close();
