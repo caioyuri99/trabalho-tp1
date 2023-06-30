4f160677bb9e5742611846fdcee6868f1fe372a3
Zanarck <caioyuri1000@gmail.com>
2023-06-30 12:19:32 -0300

    Created EmprestimoBooleanPagoFactory.java

---
+++ src/controllers/cellFactoryFormat/EmprestimoBooleanPagoFactory.java
@@ -0,0 +1,23 @@
+package controllers.cellFactoryFormat;
+
+import biblioteca.Emprestimo;
+import javafx.scene.control.TableCell;
+
+public class EmprestimoBooleanPagoFactory extends TableCell<Emprestimo, Boolean> {
+
+    @Override
+    protected void updateItem(Boolean item, boolean empty) {
+        super.updateItem(item, empty);
+
+        if (empty) {
+            setText("Sem multa");
+        } else {
+            if (item) {
+                setText("Pago");
+            } else {
+                setText("Não pago");
+            }
+        }
+    }
+
+}
