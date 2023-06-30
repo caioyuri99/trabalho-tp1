33d81ee47c101e0a2abfe671520c3145612fd099
Zanarck <caioyuri1000@gmail.com>
2023-06-30 12:20:43 -0300

    created EmprestimoItemObraNomeFactory.java

---
+++ src/controllers/cellFactoryFormat/EmprestimoItemObraNomeFactory.java
@@ -0,0 +1,20 @@
+package controllers.cellFactoryFormat;
+
+import biblioteca.Emprestimo;
+import biblioteca.Item;
+import javafx.scene.control.TableCell;
+
+public class EmprestimoItemObraNomeFactory extends TableCell<Emprestimo, Item> {
+
+    @Override
+    protected void updateItem(Item item, boolean empty) {
+        super.updateItem(item, empty);
+
+        if (empty) {
+            setText(null);
+        } else {
+            setText(item.getObra().getNome());
+        }
+    }
+
+}
